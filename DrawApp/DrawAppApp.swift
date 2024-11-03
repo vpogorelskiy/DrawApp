ccef0e56db0b05de5cd9cc2cf1be938472e238f8
Vyacheslav Pogorelskiy <vpogorelskiy@yandex-team.ru>
2024-11-03 14:52:35 +0100

    Created project

---
+++ DrawApp/DrawAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawAppApp.swift
+//  DrawApp
+//
+//  Created by Vyacheslav Pogorelskiy on 03.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct DrawAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
